2222ee20938a5c12956d658b1283d3b52ab37f2e
yyang768osu <yyang.ohiostate@gmail.com>
2019-03-26 20:55:53 -0700

    add comment for freq.h

--- src/freq.h
+++ src/freq.h
@@ -32,15 +32,23 @@
 #define OVERLAP_SIZE_5MS (2)
 #define TRAINING_OFFSET_5MS (1)
 
-#define WINDOW_SIZE_5MS (FRAME_SIZE_5MS + OVERLAP_SIZE_5MS)
+#define WINDOW_SIZE_5MS (FRAME_SIZE_5MS + OVERLAP_SIZE_5MS) 
+// 4
 
-#define FRAME_SIZE (80*FRAME_SIZE_5MS)
-#define OVERLAP_SIZE (80*OVERLAP_SIZE_5MS)
-#define TRAINING_OFFSET (80*TRAINING_OFFSET_5MS)
-#define WINDOW_SIZE (FRAME_SIZE + OVERLAP_SIZE)
-#define FREQ_SIZE (WINDOW_SIZE/2 + 1)
+#define FRAME_SIZE (80*FRAME_SIZE_5MS) 
+// 160
+#define OVERLAP_SIZE (80*OVERLAP_SIZE_5MS) 
+// 160
+#define TRAINING_OFFSET (80*TRAINING_OFFSET_5MS) 
+// 80
+#define WINDOW_SIZE (FRAME_SIZE + OVERLAP_SIZE) 
+// 320
+#define FREQ_SIZE (WINDOW_SIZE/2 + 1) 
+// 161
 
-#define NB_BANDS 18
+// everything with _5MS is divided by 80?
+
+#define NB_BANDS 18 
 #define NB_BANDS_1 (NB_BANDS - 1)
 
 void compute_band_energy(float *bandE, const kiss_fft_cpx *X);
